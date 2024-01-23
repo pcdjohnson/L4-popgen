8416365e8d4bbd5936f4607ea870bbf9a239935e
Paul Johnson <pcdjohnson@gmail.com>
2024-01-23 17:08:08 +0000

    Various minor edits, additional explanation

--- HWE_class_version.R
+++ HWE_class_version.R
@@ -9,7 +9,7 @@ library(viridis)
 
 # choose plotting options
 
-# plot a graphic of the genotypes in each population at the final generation?
+# plot a graphic of the genotypes in each sub-population at the final generation?
 plot.genotypes <- TRUE
 # slow down genetic drift plotting to see one popn at a time
 plot.delay <- 0.2 # change to e.g. 0.2 to slow down
@@ -19,13 +19,13 @@ plot.delay <- 0.2 # change to e.g. 0.2 to slow down
 # no of diploid individuals
 n <- 10 
 # number of generations to simulate
-n.gen <- 15 
+n.gen <- 15
 # frequency of A1 allele at generation 0
 p <- 0.5 
 # frequency of A2 allele at generation 0
 q <- 1 - p
-# number of populations to simulate
-n.pop <- 16 
+# number of sub-populations to simulate
+n.subpop <- 16
 # selection coefficient against A1A1 (selection against A1A2 is s/2)
 s <- 0 # try e.g. 0.05 
 # additional selection against heterozygotes
@@ -39,7 +39,7 @@ s.het <- 0
 # create a template data frame to store genotypes
 pop <- expand.grid(id = 1:n, gen = 0:n.gen, allele1 = NA, allele2 = NA)
 
-# initialise populations with exact allele frequency
+# initialise founder population with exact allele frequency
 pop[pop$gen == 0, c("allele1", "allele2")] <- 
   sample(c(rep("A1", round(p * n * 2)), rep("A2", round((1 - p) * n * 2))))
 
@@ -49,7 +49,7 @@ pop[pop$gen == 0, ]
 # simulate evolution forwards in time
 start.time <- Sys.time()
 simres <-
-  lapply(1:n.pop, function(j) {
+  lapply(1:n.subpop, function(j) {
 
     print("Generation 0", quote = FALSE)
     print(pop[pop$gen == 0, c("allele1", "allele2")], row.names = FALSE)
@@ -89,7 +89,7 @@ simres <-
     rowSums(frequencies[, c("A1A1.exp", "A1A2.exp", "A2A2.exp")])
     rowSums(frequencies[, c("A1A1.obs", "A1A2.obs", "A2A2.obs")])
     
-    #message_parallel(paste0(100 * (j/n.pop), "%"))
+    #message_parallel(paste0(100 * (j/n.subpop), "%"))
     
     return(list(pop = pop, frequencies = frequencies))
   })
@@ -111,15 +111,15 @@ axis(2, pos = 0)
 axis(2, pos = n.gen + gap)
 axis(4)
 mtext("Freq. of 'A1A2' genotype", side = 4, line = 2.5)
-title(paste("Population size =", n))
+title(paste("Sub-population size =", n))
 legend("topright", c("Expected", "Observed"), lty = 1:2,
        col = grey(0.7), bty = "n", lwd = lwd)
 
-cols <- viridis(n.pop + 1, alpha = 0.7, option = "turbo")
+cols <- viridis(n.subpop + 1, alpha = 0.7, option = "turbo")
 
-lapply(1:n.pop, function(i) {
+lapply(1:n.subpop, function(i) {
   x <- simres[[i]]
-  Sys.sleep(min(plot.delay, 2/n.pop))
+  Sys.sleep(min(plot.delay, 2/n.subpop))
   lines(x = 0:n.gen, y = x$frequencies$A1, 
         col = cols[i], lwd = lwd)
   lines(x = (0:n.gen) + gap + n.gen, y = x$frequencies$A1A2.exp, 
@@ -137,18 +137,21 @@ finish.time - start.time
 
 
 if(plot.genotypes) {
-  old.par <- par(mfrow = c(ceiling(sqrt(n.pop)), ceiling(sqrt(n.pop))), mar = c(0.3, 0.3, 0.3, 0.3))
+  old.par <- 
+    par(mfrow = c(ceiling(n.subpop/ceiling(sqrt(n.subpop))), 
+                  ceiling(sqrt(n.subpop))), 
+        mar = c(0.3, 0.3, 0.3, 0.3))
   final.genotypes.list <- 
-    lapply(1:n.pop, function(i) {
+    lapply(1:n.subpop, function(i) {
       pop <- simres[[i]]$pop
       out <- pop[pop$gen == n.gen, c("allele1", "allele2")]
       plot(1, type = "n", xlim = 0:1, ylim = 0:1, axes = FALSE)
       rect(par("usr")[1], par("usr")[3],
            par("usr")[2], par("usr")[4],
-           col = cols[i]) # Color
+           col = cols[i])
       
       box()
-      gap <- 0.05 * sqrt(n.pop)/3
+      gap <- 0.05 * sqrt(n.subpop)/3
       coords <- 
         expand.grid(x = (1:ceiling(n/2))/(ceiling(n/2)+1), 
                     y = (1:ceiling(n/2))/(ceiling(n/2)+1))
@@ -170,47 +173,76 @@ if(plot.genotypes) {
   final.genotypes <- do.call("rbind.data.frame", final.genotypes.list)[, c("allele1", "allele2")]
   
   
-  # now we can estimate some ppopulation genetic parameters
-  
-  # frequency of the p allele
+  # Now we can estimate some population genetic parameters, and use them to answer
+  # questions relevant to conservation:
+  #  1. Are the sub-populations genetically isolated from each other?
+  #  2. If so, 
+  #     (a) how long have they been isolated for and 
+  #     (b) what is the (effective) size of sub-populations?
+
+  # Frequency of the A1 allele across the whole population
   p.est <- mean(unlist(final.genotypes) == "A1")
+  print(paste("The frequency of the A1 allele across all", n.subpop, "sub-populations is", round(p.est, 3)))
   
-  # expected heterozygosity
-  He <- 2 * p.est * (1 - p.est)
+  # Expected heterozygosity
+  He.est <- 2 * p.est * (1 - p.est)
+  print(paste("The expected heterozygosity across all", n.subpop, "sub-populations is", round(He.est, 3)))
   
-  # observed homozygosity
+  # Observed homozygosity
   n.homozygote <- sum(apply(final.genotypes, 1, function(x) length(unique(x))) == 2)
-  Ho <- n.homozygote / (n * n.pop)
+  Ho.est <- n.homozygote / (n * n.subpop)
+  print(paste("The observed heterozygosity across all", n.subpop, "sub-populations is", round(Ho.est, 3)))
   
-  # now we can calculate an estimate of F, the inbreeding coefficient
-  F.est <- 1 - Ho / He
+  
+  # Now we can calculate an estimate of F, the inbreeding coefficient.
+  # If Ho is less than He, then F should be > 0.
+  F.est <- 1 - Ho.est / He.est
+  print(paste("Estimated F:", round(F.est, 3)))
   
   # Here F.est means "an estimate of F". F simply gauges the extent of 
   # homozygote excess (how many more homozygotes are there compared to the HWE expectation?).
   # Normally we would break F down into Fis, the portion of F due to inbreeding 
-  # (between individuals within populations) and Fst, the portion of F due to divergence 
-  # between populations (population structure). For the sake of simplicity, here I am 
-  # assuming no inbreeding (Fis = 0) and assuming all of F is due to population structure
-  # (F = Fst). We know this assumption is true only because we have simulated the data. 
+  # (between individuals within sub-populations) and Fst, the portion of F due to divergence 
+  # between sub-populations (population structure). For the sake of simplicity, here I am 
+  # assuming no inbreeding (Fis = 0) and assuming all of F (the homozygote excess) is due
+  # to population structure (F = Fst). 
+  # 
+  # Why do we expect isolation between sub-populations to lead to fewer homozygotes 
+  # than expected and therefore high Fst? Perhaps this is easier to imagine if we take
+  # an extreme case. Make the following changes near the top of the code:
+  #   Set plot.genotypes to be TRUE, not FALSE
+  #   Change the number generations (n.gen) to 100
+  # Now run the code by clicking on "Source".
+
+  # You should now see a diagram of all the sub-populations as different coloured 
+  # squares, with individuals as black rectangles, and with each individual showing its
+  # genotype. Probably most of the sub-populations will be full of either A1A1 homozygotes, 
+  # or A2A2 homozygotes, with very few or no heterozygotes. This is because mating 
+  # is not random among the whole population but restricted to within sub-populations
+  
+  
+  
+  
+  # We know this assumption is true only because we have simulated the data. 
   # *** You would never normally assume F = Fst, I'm only doing this to simplify this code ***
   # 
   # What does Fst mean, in practical terms? I.e. what can we learn about 
-  # the "real" populations from our Fst estimate?
-  # To do that we have to make some assumptions about the populations,
+  # the "real" sub-populations from our Fst estimate?
+  # To do that we have to make some assumptions about the population,
   # which taken together will form a model.
   # Let's assume that: 
-  #   the populations are diverging independently from a common ancestor with no migration
-  #   all the populations are the same constant size
+  #   the sub-populations are diverging independently from a common ancestor with no migration
+  #   all the sub-populations are the same constant size
   #   no mutation
   #   no selection
-  #   the populations have not reached equilibrium (think about why this assumption is necessary)
+  #   the sub-populations have not reached equilibrium (think about why this assumption is necessary)
   #   random mating
   #   non-overlapping generations
   # If all of the above apply, the following equation holds:
   #
   #   Fst = 1 - (1 - 1/(2*n))^t
   # 
-  # where n is the size of the populations and t is the number of generations since divergence.
+  # where n is the size of the sub-populations and t is the number of generations since divergence.
   #
   # (My source for this equation was equations s1 and s2 of this paper:
   # https://doi.org/10.1101%2Fgr.154831.113
@@ -224,9 +256,9 @@ if(plot.genotypes) {
   print(paste("'True' F:", round(1 - (1 - 1/(2*n))^n.gen, 3)))
   print("***********************************")
   
-  # If this was real data, and we knew the (effective) size of each population,
+  # If this was real data, and we knew the (effective) size of each sub-population,
   # we could solve the above equation for t (time in generations), and estimate
-  # the age of the populations in no of generations since divergence:
+  # the age of the sub-populations in no of generations since divergence:
   t.est <- log(1 - F.est) / log((1 - 1/(2*n)))
   print("***********************************")
   print(paste("Estimated no of generations since splitting:", round(t.est, 1)))
@@ -235,12 +267,12 @@ if(plot.genotypes) {
   #
   # Alternatively, if we know the number of generations since splitting
   # (which is probably more realistic) we can estimate the effective size
-  # of each population.
+  # of the sub-populations.
   # Solve the above equation for n:
   n.est <- 1/(2*(1 - (1 - F.est)^(1/n.gen)))
   print("***********************************")
-  print(paste("Estimated effective population size (Ne):", round(n.est, 1)))
-  print(paste("True effective population size (Ne):", n))
+  print(paste("Estimated effective sub-population size (Ne):", round(n.est, 1)))
+  print(paste("True effective sub-population size (Ne):", n))
   print("***********************************")
   
 }
